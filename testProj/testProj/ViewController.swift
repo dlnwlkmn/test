5e72898aaf3f74b62e78119eee3d8bb88c6cb993
Alexey <lesh77906@gmail.com>
2020-09-28 23:23:17 +0300

    Init commit xcodeprij

---
+++ testProj/testProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testProj
+//
+//  Created by Алексей Савельев on 28.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
